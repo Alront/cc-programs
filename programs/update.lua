e32880942ec7440feacc20e2071d30d5e47cb21e
Alront <alron222@gmail.com>
2020-07-20 15:38:58 +0200

    Lorem ipsum

--- programs/update.lua
+++ programs/update.lua
@@ -2,4 +2,4 @@ shell.run("github Alront cc-programs . programs master")
 if fs.exists("/programs") then
     fs.delete("/programs")
 end
-fs.move("/downloads/cc-programs/programs", ".")
+fs.move("/downloads/cc-programs/programs", "/prgs")
