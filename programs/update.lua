b1256b99a9a3ced9e8f12fa9c8b6cc688055fbdc
Alront <alron222@gmail.com>
2020-07-20 15:46:26 +0200

    Lorem ipsum

--- programs/update.lua
+++ programs/update.lua
@@ -2,7 +2,7 @@ if fs.exists("/downloads/cc-programs") then
     fs.delete("/downloads/cc-programs")
 end
 shell.run("github Alront cc-programs . programs master")
-if fs.exists("/programs") then
-    fs.delete("/programs")
+if fs.exists("/p") then
+    fs.delete("/p")
 end
-fs.move("/downloads/cc-programs/programs", "/prgs")
+fs.move("/downloads/cc-programs/programs", "/p")
